0e41b293f3e6ffa81205b71738a62ac0b1e61ec1
David Espinola <daver787@gmail.com>
2019-09-15 09:35:18 -0500

    made changes to titles and legends for figures.

--- HW1/hw1_script.m
+++ HW1/hw1_script.m
@@ -24,11 +24,12 @@ hold on
 plot(index_max_nino3,max_nino3,'*','Color','black')
 plot(index_min_nino3,min_nino3,'*','Color','black')
 legend('nino3','nino3.4','max','min')
-title('SST over time')
+title('Figure 1')
 
 % 3)compute a 2-panel plot of histogram of Nino3 using Matlab function
 % histogram
 figure
+sgtitle('Figure 2')
 subplot(2,1,1);
 histogram(nino_3);
 xlabel('Temperature(deg C)');
@@ -38,20 +39,26 @@ histogram(nino_3,28);
 xlabel('Temperature(deg C)');
 ylabel('Frequency');
 
+
 %4)Create a scatter plot of Nino3.4(y-axis) vs Nino3(x-axis)
 figure
 scatter(nino_3,nino3_4);
-axis([24 30 24 30]);
+axis([24 30 24 30])
+xlabel('Nino3 Temperature');
+ylabel('Nino 3.4 Temperature');
+title('Figure 3');
 
 %5) Create a one panel time series of SST anomalies
 figure
 nino_3_anom=data(:,6);
 nino3_4_anom=data(:,10);
-plot(i,nino_3_anom,'Color','blue');
+b1=plot(i,nino_3_anom,'Color','blue');
 hold on
-plot(i,nino3_4_anom,'--','Color','red');
+b2=plot(i,nino3_4_anom,'--','Color','red');
 legend('nino3 anomalies','nino3.4 anomalies');
-title('SST anomalies over time');
+xlabel('Time(months)')
+ylabel('Temperature(deg C)')
+title('Figure 4');
 
 %6) Create a one figure seasonal cycle from difference
 figure
@@ -60,14 +67,14 @@ diff_nino3_4=nino3_4-nino3_4_anom;
 plot(i,diff_nino3,i,diff_nino3_4)
 ylim([22,30])
 xlabel('Time(months)')
-ylabel(' difference SST(deg C)');
+ylabel(' difference SST(deg C)')
+title('Figure 5');
 
 %7)Create a map of the world using coastmap
-%landscape();
 figure
-%portrait();
-%landscape;
-hold on
+landscape();
+coastmap([0 360 -60 60],40,'fill');
 boxdraw([210,270,-5,5],'blue');
 boxdraw([190,240,-5,5],'red','r--');
-coastmap([0 360 -60 60],40,'fill');
+%legend('Nino 3','Nino 3.4')
+title('Figure 6');
