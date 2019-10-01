f0e6ea413d791ea155c2e358c5fee4f782f51b69
David Espinola <daver787@gmail.com>
2019-10-01 09:47:15 -0500

    Finished homework 4 script.

--- HW4/hw4_script.m
+++ HW4/hw4_script.m
@@ -12,14 +12,36 @@ axis([1980 2012 23 30]);
 
 %compute the long term mean seasonal cycle for nino3 using the 30 year base
 M= zeros(30,12);
-count=1
+count=1;
 for i=(1:30)
     M(i,:)=nino3(count:(12*i));
     count=count+12;
-end 
+end
 
-N=mean(M)
-P=repmat(N,[1,30]);
+%use repmeat to plot periodic seasonal cycle from 1981-2010
+N=mean(M);
+p=repmat(N,[1,30]);
 figure;
-plot(time_vector,P);
+plot(time_vector,p);
+xlabel('Time(months)');
+ylabel('SST(deg C)');
 axis([1980 2012 23 30]);
+
+%compute Nino3 SST anomaly time series by subtracting periodic trend
+no_seasonal=(nino3'-p)';
+figure;
+plot(time_vector,no_seasonal);
+xlabel('Time(months)');
+ylabel('SST(deg C)');
+axis([1980 2012 -2 4]);
+
+%plot Nino3 anomaly time series and our computed anomaly time series on
+%same plot
+figure
+nino3_anom=dat(:,6);
+plot(time_vector,nino3_anom,'Color','red');
+xlabel('Time(months)');
+ylabel('SST(deg C)');
+hold on
+plot(time_vector,no_seasonal,'+','Color','blue');
+axis([1980,2012,-2,-4]);
