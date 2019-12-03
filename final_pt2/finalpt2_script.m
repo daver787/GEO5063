e718110c99eef2e0d86e0d51d8f5f494e5e4383d
David Espinola <daver787@gmail.com>
2019-12-02 21:41:30 -0600

    fixed discrepancy that printed interpolated points on different figure.

--- final_pt2/finalpt2_script.m
+++ final_pt2/finalpt2_script.m
@@ -16,43 +16,37 @@ plot(t,y,'o','markerfacecolor','b');
 
 
 %estimate chlorophyll at intermediate times by linear interploation and
-%plot
-figure;
+%plot using red open circles
 ti=1.5:1:143.5;
 yi=interp1(t,y,ti);
-plot(ti,yi,'o','markerfacecolor','r');
-xlabel('Time (index number)');
-ylabel('Chlorophyll(mg/m^3)');
-title('Figure 2');
+hold on;
+scatter(ti,yi,'r');
 
 %repeat using spline interpolation and plot in same figure using green open
 %circles
 si=spline(t,y,ti);
 hold on;
-plot(ti,si,'o','markerfacecolor','g');
-legend('linear interpolation','cubic spline interpolation');
+scatter(ti,si,'g');
+legend('time series','original data points','linear interpolation','cubic spline interpolation');
 
 %repeat original plot with line width equal to 2
 figure;
 plot(t,y,'linewidth',[2]);
-xlabel('Time (index number)');
-ylabel('Chlorophyll(mg/m^3)');
-title('Figure 3');
 
 %perform a 3 point running mean
 ys3=movmean(y,3);
 
-%plot the result as a red soline line with line width equal to 2
+%plot the result as a red solid line with line width equal to 2
 hold on;
 plot(t,ys3,'r','linewidth',[2]);
 
 %perform a 7 point running mean
 ys7=movmean(y,7);
 
-%plot the result as a green solid line
+%plot the result as a green solid line with line width equal to 2
 hold on;
 plot(t,ys7,'g','linewidth',[2]);
 xlabel('Time (index number)');
 ylabel('Chlorophyll(mg/m^3)');
-title('Figure 3');
+title('Figure 2');
 legend('No smoothing','3 point moving average','7 point moving average');
